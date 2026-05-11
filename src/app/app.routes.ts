850552bff4076d63d4a17deb9b25ae943ad5b9ba
AGUSTIN CALLATA <agus.callata@gmail.com>
2026-05-11 16:02:21 -0300

    correccion punto4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { Punto1 } from './components/puntos/punto1/punto1';
 import { Punto2 } from './components/puntos/punto2/punto2';
 import { Punto3 } from './components/puntos/punto3/punto3';
 import { Punto4 } from './components/puntos/punto4/punto4';
-import { Punto5 } from './components/puntos/punto5/punto5';
 
 export const routes: Routes = [
     {
@@ -31,9 +30,5 @@ export const routes: Routes = [
     {
         path: "punto4",
         component: Punto4
-    },
-    {
-        path: "punto5",
-        component: Punto5
     }
 ];
